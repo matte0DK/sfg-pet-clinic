df569576ff6c1d7003ee1fcf4cfdf2380ffc6240
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-07 10:53:04 +0200

    Implemented POJO's

---
+++ src/main/java/matteo/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package matteo.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/matteo/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package matteo.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
