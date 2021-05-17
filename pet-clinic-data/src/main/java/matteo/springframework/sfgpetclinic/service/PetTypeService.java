638201e1607141990b42859db80f55a88daf67ac
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-17 09:47:21 +0200

    added PetTypeService, closes #31

---
+++ pet-clinic-data/src/main/java/matteo/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package matteo.springframework.sfgpetclinic.service;
+
+import matteo.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
