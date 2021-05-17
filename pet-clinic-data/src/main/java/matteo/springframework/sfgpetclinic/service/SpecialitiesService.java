298ba9bde470da141af30c28699afcfbe2c0bb0a
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-17 10:40:21 +0200

    added specialities map impl, closes #33

---
+++ pet-clinic-data/src/main/java/matteo/springframework/sfgpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package matteo.springframework.sfgpetclinic.service;
+
+import matteo.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
